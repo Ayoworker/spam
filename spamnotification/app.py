a44bc8a2aec5800e54d1be4d2bb9da1f37a36134
Ayoworker <152839837+Ayoworker@users.noreply.github.com>
2024-05-17 10:35:09 +0200

    Update app.py

--- spamnotification/app.py
+++ spamnotification/app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import joblib
+import pandas as pd
+import random
 
 # Load the serialized model
-model = joblib.load("spamnotification/spam_classification_pipeline.pkl")
+model = joblib.load("spam_classification_pipeline.pkl")
 
+# Load the CSV file containing messages
+messages_df = pd.read_csv("new_spam.csv")
 
 # Function to detect spam
 def detect_spam(message):
@@ -11,17 +15,28 @@ def detect_spam(message):
     prediction = model.predict([message])[0]
     return prediction
 
+# Function to generate a random message from the CSV file
+def generate_random_message():
+    random_index = random.randint(0, len(messages_df) - 1)
+    random_message = messages_df.iloc[random_index]["v2"]
+    return random_message
 
 # Main function to run the app
 def main():
     # Set the title of the web app
     st.title("Spam Detector")
 
-    # Get input message from the user
-    message = st.text_input("Enter your message:")
+    # Initialize session state
+    if "message" not in st.session_state:
+        st.session_state.message = ""
 
-    # Initialize is_spam variable
-    is_spam = False
+    # Text input for user message
+    message = st.text_input("Enter your message:", value=st.session_state.message)
+
+    # Generate Random Message button
+    if st.button("Generate Random Message"):
+        random_message = generate_random_message()
+        st.session_state.message = random_message
 
     # Detect spam when the user presses Enter or clicks the button
     if st.button("Detect"):
@@ -37,10 +52,9 @@ def main():
 
             # Display result
             if is_spam:
-                st.write("It's likely a scam")
+                st.error("It's likely a scam")
             else:
-                st.write("You are safe")
-
+                st.success("You are safe")
 
 # Run the app
 if __name__ == "__main__":
